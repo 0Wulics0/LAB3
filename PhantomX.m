5040f3b220ec9696edfc3cef4530bfbeb7c495f7
0Wulics0 <111406451+0Wulics0@users.noreply.github.com>
2022-10-18 21:03:52 -0500

    Imágen DH

--- PhantomX.m
+++ PhantomX.m
@@ -27,12 +27,13 @@ for i=1:length(q_deg)
     call(motorSvcClient,motorCommandMsg);
 end
 end
+%%
 l=[14.5,10.7,10.7 9];
 L(1) = Link('revolute','alpha',pi/2,'a',0,'d',l(1),'offset',0);
 L(2) = Link('revolute','alpha',0,'a',l(2),'d',0,'offset',pi/2);
 L(3) = Link('revolute','alpha',0,'a',l(3),'d',0,'offset',0);
 L(4) = Link('revolute','alpha',0,'a',0,'d',0,'offset',0);%%
-Pincher=SerialLink(L,'name','PX');
+Pincher=SerialLink(L,'name','PX')
 Pincher.tool=[0 0 1 l(4);-1 0 0 0;0 -1 0 0;0 0 0 1];
 figure
 Pincher.plot(q_rad,'notiles','noname','floorlevel',-1);
